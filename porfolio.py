284639788b4b9d542d03bb7b402a3fb815acd810
kdiop8059-ux <kdiop8059@gmail.com>
2026-02-17 21:50:30 +0000

    Add files via upload

---
+++ porfolio.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+with st.sidebar:
+     st.write("mame khary diop")
+     st.write("767299656")
+     st.write("kdiop8059@gmail.com")
+    
+st.title("Porfolio_Géomatique")
+
+st.header("profil")
+st. markdown("""
+Technicienne superieur en géomatique et etudiate en licence 3 de géographie.
+Je dispose de solide base en cartographie et systéme d'information géographique . Rigoureuse et engagée,je suis déterminée a valoriser mon savoir faire et a developper d'avantage mes compétences dans un envirronnement professionnel.
+""")
+
+st.subheader("formation")
+st.markdown("- BTS en géomatique")
+st.markdown("-Licences 3 Géomatique(en cours)")
+st.markdown("-Baccalaureat")
+
+st.subheader ("competence")
+st.markdown("-Travaux pratiques en systeme d'information géographique (SIG) :QGIS,ARCGIS")
+st.markdown("-Collecte et traitement de donnée ")
+st.markdown("_Topographie(station totale et mobil topo)")
+st.markdown("_Modélisation2D /3D (autocadet sketchup)")
+st.markdown("-Maitrise des outils bureautique(world,excel)")
+
+
+st.subheader("experiences")
+st.markdown("_Numerisation et géoreferencement des données sur QGIS et ARCQIS")
+st.markdown("_Conseption et mise en page des cartes")
+st.markdown("_collecte de donnée avec mobiltopo et station total")
